aacbada2eb638c9859f4cce15390bac6f164d176
Tomoaki Yagishita <tyagishi@gmail.com>
2021-01-25 15:44:36 +0900

    as initial

--- Sources/SDSNSImageExtension/SDSNSImageExtension.swift
+++
@@ -1,3 +0,0 @@
-struct SDSNSImageExtension {
-    var text = "Hello, World!"
-}
--- Tests/SDSNSImageExtensionTests/SDSNSImageExtensionTests.swift
+++ Tests/SDSNSImageExtensionTests/SDSNSImageExtensionTests.swift
@@ -6,7 +6,8 @@ final class SDSNSImageExtensionTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(SDSNSImageExtension().text, "Hello, World!")
+        
+        XCTAssertTrue(false) // add test
     }
 
     static var allTests = [
