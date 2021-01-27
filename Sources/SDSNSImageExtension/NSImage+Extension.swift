4aa6297faf420730edaef09991230dcdaedef5eb
Tomoaki Yagishita <tyagishi@gmail.com>
2021-01-27 17:04:54 +0900

    ok to save image file with metadata(and modified data if necessary)

--- Sources/SDSNSImageExtension/NSImage+Extension.swift
+++ Sources/SDSNSImageExtension/NSImage+Extension.swift
@@ -49,7 +49,7 @@ extension NSImage {
         return CGSize(width: width, height: height)
     }
     
-    public func unscaledBitmapImageRep(_ targetSize:CGSize, drawHandler: @escaping () -> () ) -> NSBitmapImageRep {
+    public func unscaledBitmapImageRep(_ targetSize:CGSize, drawHandler: (() -> ())? ) -> NSBitmapImageRep {
         guard let bitmapRep = NSBitmapImageRep(bitmapDataPlanes: nil, pixelsWide: Int(targetSize.width), pixelsHigh: Int(targetSize.height),
                                          bitsPerSample: 8, samplesPerPixel: 4, hasAlpha: true, isPlanar: false,
                                          colorSpaceName: .calibratedRGB, bytesPerRow: 0, bitsPerPixel: 0) else {
@@ -59,8 +59,58 @@ extension NSImage {
         
         NSGraphicsContext.saveGraphicsState()
         NSGraphicsContext.current = NSGraphicsContext(bitmapImageRep: bitmapRep)
-        drawHandler()
+        drawHandler?()
         NSGraphicsContext.restoreGraphicsState()
         return bitmapRep;
+    }
+}
+
+extension NSImage {
+    public func jpegDataWithMetadata(_ image: NSImage, _ imageURL: URL) -> Data?{
+        // copy source property
+        guard let cgImageSource = CGImageSourceCreateWithURL(imageURL as CFURL, nil) else { print("failed to create")
+            return nil
+        }
+        // let props = CGImageSourceCopyProperties(cgImageSource, nil) // NG: failed to get
+        let sourceProps = CGImageSourceCopyPropertiesAtIndex(cgImageSource, 0, nil)
+
+        let destData = NSMutableData()
+        let cgImageDestination = CGImageDestinationCreateWithData(destData as CFMutableData, kUTTypeJPEG, 1, nil)!
+
+        // test dic
+        var testDic = Dictionary<String,Any>()
+        testDic[kCGImagePropertyExifAuxSerialNumber as String] = NSNumber(2828)
+//        CGImageDestinationSetProperties(cgImageDestination, testDic as CFDictionary)
+
+        var rect = CGRect(x: 50, y: 50, width: 200, height: 200)
+        let cgImage = image.cgImage(forProposedRect: nil, context: nil, hints: nil)!
+        let writeCGImage = cgImage.cropping(to: rect)!
+        //var dic = sourceProps as? Dictionary<String,Any>
+        var dic = sourceProps as? Dictionary<String,Any>
+        if dic != nil {
+            // always NSImage has .up
+            dic![kCGImagePropertyOrientation as String] = CGImagePropertyOrientation.up
+            
+            if var exif = dic![kCGImagePropertyExifDictionary as String] as? Dictionary<String,Any> {
+                
+                exif[kCGImagePropertyExifUserComment as String] = "Hello from ExifClip"
+                //exif[kCGImagePropertyExifPixelXDimension as String] = NSNumber(333)
+                print("exit exists!")
+//                //print(dic![kCGImagePropertyExifPixelXDimension as String] as? NSNumber )
+//                //dic![kCGImagePropertyExifAuxSerialNumber as String] = NSNumber(2929)
+//                //dic![kCGImagePropertyExifPixelXDimension as String] = NSNumber(300)
+                // need to copy-back to original dictionary
+                dic![kCGImagePropertyExifDictionary as String] = exif
+            }
+//            dic[kCGImagePropertyOrientation as String] = CGImagePropertyOrientation.up
+        }
+        //let check = dic[kCGImagePropertyOrientation]
+        CGImageDestinationAddImage(cgImageDestination, writeCGImage, dic! as CFDictionary)
+        //CGImageDestinationSetProperties(cgImageDestination, dic! as CFDictionary)
+        
+        //print(sourceProps)
+        CGImageDestinationFinalize(cgImageDestination)
+        
+        return destData as Data
     }
 }
